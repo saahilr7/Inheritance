e7c227602f74024e0537373e07485cb3d4b3fe4e
Your Name <you@example.com>
2020-02-07 12:19:55 -0500

    success

---
+++ In-class_Inheritance/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace In_class_Inheritance
+{
+    class Car1
+    { 
+
+        public Car1()
+        {
+        model = "It is a car!";
+        }
+    
+        public string color = "red";
+        public int maxSpeed = 200;
+        public string model;
+        public int year;
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+
+    }
+}
